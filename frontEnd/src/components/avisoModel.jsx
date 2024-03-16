9a7aa577df147acfb085e63d772c61f58d34eb03
yidsuds <jaramirezc@unah.hn>
2024-03-15 20:55:15 -0600

    cambio de history

--- frontEnd/src/components/avisoModel.jsx
+++ frontEnd/src/components/avisoModel.jsx
@@ -1,36 +1,36 @@
-import React from 'react'
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; 
 
-function LoguearparaverModal({show,handleClose}){
-    const history = useHistory();
+function LoguearparaverModal({ show, handleClose }) {
+    const navigate = useNavigate(); 
 
     const handleLogin = () => {
-      history.push('/login');
+        navigate('/login'); // Utiliza navigate para redireccionar a '/login'
     };
-  
+
     return (
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton className='bc-degrate'>
-          <Modal.Title className='fw-bold text-light'>Espera por favor</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className='bc-primary text-light justify'>
-          <p className='justify'>
-            Para acceder a la información completa del producto,
-            es necesario que inicie sesión en HONDUMARKET
-          </p>
-        </Modal.Body>
-        <Modal.Footer className='bc-degrate'>
-          <Button className="btn btn-danger" onClick={handleClose}>
-            Salir
-          </Button>
-          <Button className="btn btn-primary" onClick={handleLogin}>
-            Iniciar seción
-          </Button>
-        </Modal.Footer>
-      </Modal>
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton className='bc-degrate'>
+                <Modal.Title className='fw-bold text-light'>Espera por favor</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className='bc-primary text-light justify'>
+                <p className='justify'>
+                    Para acceder a la información completa del producto,
+                    es necesario que inicie sesión en HONDUMARKET
+                </p>
+            </Modal.Body>
+            <Modal.Footer className='bc-degrate'>
+                <Button className="btn btn-danger" onClick={handleClose}>
+                    Salir
+                </Button>
+                <Button className="btn btn-primary" onClick={handleLogin}>
+                    Iniciar sesión
+                </Button>
+            </Modal.Footer>
+        </Modal>
     );
-  }
-  
-  export default LoguearparaverModal;
+}
+
+export default LoguearparaverModal;
