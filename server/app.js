35e48f5ee366819db41c81b19b0413b7ad72f446
Jeffer <manymartinez83@gmail.com>
2024-04-15 00:08:17 -0600

    Actualizando Funcionalidad de denuncias, cors actualizado

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 //midlewares
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173','http://localhost:5174'],
     credentials: true
 }));
 app.use(express.urlencoded({extended: true}));
