b84771f78f1566fe80b0267dcaad10cd1934e48b
Carlos Sosa <carloseduardososagonzalez@gmail.com>
2024-04-26 16:17:05 -0600

    correccion del los sql

--- db/procedimientos.sql
+++ db/procedimientos.sql
@@ -407,7 +407,7 @@ BEGIN
 END//
 DELIMITER ;
 
---NUEVO CAMBIADO EL 24/04/24
+-- NUEVO CAMBIADO EL 24/04/24
 -- Obtener calificaciones y comentarios de un producto
 DELIMITER //
 CREATE PROCEDURE sp_obtenerComentariosProducto(
@@ -424,10 +424,9 @@ BEGIN
 END//
 DELIMITER ;
 
---NUEVO CAMBIADO EL 24/04/24
+-- NUEVO CAMBIADO EL 24/04/24
 -- Obtener calificaciones y comentarios de un usuario
---Se borra el anterior
-drop procedure sp_obtenerComentarios
+
 
 DELIMITER //
 CREATE PROCEDURE sp_obtenerComentarios(
@@ -850,22 +849,22 @@ DELIMITER ;
 
 -- PUBLICACIONES OBTENIDAS DE LA BUSQUEDA
 DELIMITER //
-CREATE PROCEDURE sp_todasPublicacionesSearch(IN searchTerm VARCHAR(255))
+CREATE PROCEDURE sp_todasPublicacionesSearch(IN searchTerm VARCHAR(255), IN categoriaId INT)
 BEGIN
     SELECT  
-		p.producto_id AS id,
+        p.producto_id AS id,
         p.nombre_producto AS nombre,
         p.precio_producto AS precio,
         p.descripcion_producto AS descripcion,
-		p.categoria_id AS categoria,
+        p.categoria_id AS categoria,
         p.departamento_id AS departamento,
         p.fecha_publicacion
     FROM 
         tbl_productos p
     WHERE 
-        p.nombre_producto LIKE CONCAT('%', searchTerm, '%')
-       AND (p.fecha_programada IS NULL OR p.fecha_programada <= NOW())
-       AND p.producto_inactivo != 1
-       ORDER BY p.fecha_publicacion desc;
+        (p.nombre_producto LIKE CONCAT('%', searchTerm, '%') AND (p.categoria_id = categoriaId OR categoriaId IS NULL))
+        AND (p.fecha_programada IS NULL OR p.fecha_programada <= NOW())
+        AND p.producto_inactivo != 1
+        ORDER BY p.fecha_publicacion desc;
 END //
 DELIMITER ;
